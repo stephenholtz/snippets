c7454d0fbfeab29c7e796c467872cfb8d2c50676
Stephen L Holtz <git@holtz.email>
2017-05-06 12:31:25 -0400

    Update write_img_osx.sh

--- write_img_osx.sh
+++ write_img_osx.sh
@@ -1,5 +1,10 @@
 # since I forget this every time...
 diskutil list
+
+#
+sudo diskutil eraseDisk FAT32 DEMO MBRFormat /dev/disk2
 diskutil unmountDisk /dev/disk2
+
+#
 sudo dd if=/Users/stephenholtz/Downloads/pfSense-CE-memstick-2.3.3-RELEASE-i386.img of=/dev/disk2 bs=1m
 diskutil eject /dev/disk2
