1c0e8aeccca6a05a259ef77ff3fe53a6ca9f12a4
Stephen Holtz <git@holtz.email>
2017-06-13 14:02:53 -0400

    rm typo

--- jupyterlab_launch.sh
+++ jupyterlab_launch.sh
@@ -1,3 +1,7 @@
 #jllaunch.sh
-stephenholtz$ LAB_URL=$(jupyter notebook list | ruby -ne 'puts /(http:.*token=\S{48})/.match(gets)') && echo $LAB
+
+LAB_URL=$(jupyter notebook list | ruby -ne 'puts /(http:.*token=\S{48})/.match(gets)')
+
+echo $LAB_URL
+
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome -app=$LAB_URL
